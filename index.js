361778f8a3cf7cba21daa185ac72b88a317291ee
gaurav ashok ghuge <130838374+gauravghuge7@users.noreply.github.com>
2024-01-30 17:44:33 +0530

    second  commit

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.get('/', (req,res)=> {
       res.send('welcome to men and woman dummy data ')
 })
 
+app.get('/men', (req,res) => {
+      res.send(' accept 10 product data men ')
+})
+
+app.get('/woman', (req,res) => {
+      res.send('accept 10 products data of woman')
+})
+
+app.get('other', (req,res) => {
+      res.send('page not found ')
+})
+
 app.listen(port, () => {
       console.log(`app is listening on port http://localhost:${port}`)
 })
